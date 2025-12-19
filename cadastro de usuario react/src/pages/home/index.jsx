4094dc77b4adb1784711b9a48fa7cb77f942e6ae
Your Name <you@example.com>
2025-12-19 10:16:32 -0300

    cadastro usuarios react

---
+++ cadastro de usuario react/src/pages/home/index.jsx
@@ -0,0 +1,19 @@
+
+import '../home/style.css'
+
+function Home() {
+
+
+  return (
+    <>
+      <div>
+        <h1>Cadastro de Usuário</h1>
+      </div>
+        
+      
+        
+    </>
+  )
+}
+
+export default  Home
